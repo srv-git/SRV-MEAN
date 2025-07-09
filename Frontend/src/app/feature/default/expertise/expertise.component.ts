d1cfd92accc073aef63009bf82a7e6dff16970f6
Shyam Raj Verma <shyamrajverma23@gmail.com>
2025-07-09 21:25:28 +0530

    Updated about, expertise & product pages

--- Frontend/src/app/feature/default/expertise/expertise.component.ts
+++ Frontend/src/app/feature/default/expertise/expertise.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-expertise',
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './expertise.component.html',
-  styleUrl: './expertise.component.scss'
+  styleUrl: './expertise.component.scss',
 })
-export class ExpertiseComponent {
-
-}
+export class ExpertiseComponent {}
